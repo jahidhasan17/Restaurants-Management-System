3828e019801a48069c90c5acc8af98f0030da0ab
jahidhasan17 <jhasan1743@gmail.com>
2021-01-27 17:38:16 +0600

    add food section

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -8,47 +8,96 @@
 
 			<br><br>
 
-			<a href="#" class="btn-primary">Add food</a>
+			<a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add food</a>
 
 			<br /><br /><br />
+
+			<?php
+
+				if(isset($_SESSION['add'])){
+					echo $_SESSION['add'];
+					unset($_SESSION['add']);
+				}
+				if(isset($_SESSION['delete'])){
+					echo $_SESSION['delete'];
+					unset($_SESSION['delete']);
+				}
+				if(isset($_SESSION['upload'])){
+					echo $_SESSION['upload'];
+					unset($_SESSION['upload']);
+				}
+				if(isset($_SESSION['un'])){
+					echo $_SESSION['un'];
+					unset($_SESSION['un']);
+				}
+
+			?>
 
 			<table class="tbl-full">
 				<tr>
 					<th>S.N</th>
-					<th>Full Name</th>
-					<th>Username</th>
-					<th>Actions</th>
+					<th>Title</th>
+					<th>Price</th>
+					<th>Image</th>
+					<th>Feature</th>
+					<th>Active</th>
+					<th>Action</th>
 				</tr>
 
-				<tr>
-					<td>1</td>
-					<td>jahid</td>
-					<td>hasan</td>
-					<td>
-						<a href="#" class="btn-secondary">Update Admin</a>
-						<a href="#" class="btn-danger">Delete Admin</a>
-					</td>
-				</tr>
+				<?php
 
-				<tr>
-					<td>2</td>
-					<td>jahid</td>
-					<td>hasan</td>
-					<td>
-						<a href="#" class="btn-secondary">Update Admin</a>
-						<a href="#" class="btn-danger">Delete Admin</a>
-					</td>
-				</tr>
+					$sql = " SELECT * FROM tbl_food ";
+					$res = mysqli_query($conn,$sql);
 
-				<tr>
-					<td>3</td>
-					<td>jahid</td>
-					<td>hasan</td>
-					<td>
-						<a href="#" class="btn-secondary">Update Admin</a>
-						<a href="#" class="btn-danger">Delete Admin</a>
-					</td>
-				</tr>
+					$cnt = mysqli_num_rows($res);
+
+					if($cnt > 0){
+						$sl = 1;
+						while($row = mysqli_fetch_assoc($res)){
+							$id = $row['id'];
+							$title = $row['title'];
+							$price = $row['price'];
+							$image_name = $row['image_name'];
+							$featured = $row['feature'];
+							$active = $row['active'];
+							?>
+
+								<tr>
+								<td><?php echo $sl++; ?></td>
+								<td><?php echo $title; ?></td>
+								<td><?php echo $price; ?></td>
+								<td>
+									<?php 
+
+										if($image_name == ""){
+											echo "<div class = 'error'>Image Not Added</div>";
+										}else{
+											?>
+											<img src="<?php echo SITEURL;?>images/food/<?php echo $image_name;?>" width = "100px">
+											<?php
+										}
+
+									?>
+								</td>
+								<td><?php echo $featured; ?></td>
+								<td><?php echo $active; ?></td>
+								<td>
+									<a href="#" class="btn-secondary">Update Food</a>
+									<a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id;?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
+								</td>
+							</tr>
+
+							<?php
+						}
+
+					}else{
+						echo "<tr><td colspan = '7' class = 'error'> Food not Added Yet.</td></tr>";
+						//echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
+					}
+
+				?>
+
+				
 			</table>
 
 
